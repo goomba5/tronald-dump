7f3f9f6bbb498ad5b36ff85c26577d81dfdd9cd2
Gary Henderson <hendergy@gmail.com>
2020-02-23 18:30:44 -0500

    removed default styles and prototypes

--- src/components/header.js
+++ src/components/header.js
@@ -9,34 +9,20 @@ const Header = ({ siteTitle }) => (
       marginBottom: `1.45rem`,
     }}
   >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
+    <div>
+      <h1>
         <Link
           to="/"
           style={{
             color: `white`,
-            textDecoration: `none`,
+            textDecoration: `none`
           }}
         >
-          {siteTitle}
+          Tronald Dump Quotes
         </Link>
       </h1>
     </div>
   </header>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
