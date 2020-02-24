aab6f6ea8de91bf0cdafee5f464049ea5f68cc7e
Gary Henderson <hendergy@gmail.com>
2020-02-23 21:13:48 -0500

    added Quote component to the home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import React from "react"
 
 import Layout from "../components/layout"
+import Quote from "../components/quotes"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <Quote/>
     <p>You are reading random quotes from a used orange peel. Enjoy.</p>
   </Layout>
 )
