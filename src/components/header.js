842831340e2632b175ae322b25b6a11179372fc1
Gary Henderson <hendergy@gmail.com>
2020-02-24 22:47:08 -0500

    removed inline css styles for Header, updated layout stylesheet for Header

--- src/components/header.js
+++ src/components/header.js
@@ -1,27 +1,11 @@
-import { Link } from "gatsby"
 import React from "react"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
+const Header = () => (
     <div>
-      <h1>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`
-          }}
-        >
-          Tronald Dump Quotes
-        </Link>
+      <h1 className="header">
+        Tronald Dump Quotes
       </h1>
     </div>
-  </header>
 )
 
 export default Header
